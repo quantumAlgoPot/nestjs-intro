5ecfc3aa158ce2ffc314fbcdc8ddeee183d1a6e3
Bilal Akbar <bilal.akbar@kryptomind.com>
2022-01-10 16:35:42 +0500

    Added Nest Fastify

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,22 @@ import * as helmet from 'helmet';
 import * as csurf from 'csurf';
 import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 declare const module: any;
 const consoleService = new ConsoleService();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /* const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  ); */
   app.enableCors();
   app.enableVersioning();
   app.useGlobalInterceptors();
-  app.use(csurf()); // Cross-site request forgery (also known as CSRF or XSRF)
   app.use(cookieParser()); // For Cookies
   app.use(
     // for Session
@@ -23,13 +30,14 @@ async function bootstrap() {
       saveUninitialized: false,
     }),
   );
+  app.use(csurf()); // Cross-site request forgery (also known as CSRF or XSRF)
   // app.use(helmet());
-  await app.listen(3000);
 
   if (module.hot) {
     consoleService.print('Hot Reload in Use');
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
