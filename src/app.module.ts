99694406a20438a8c252ec0f374f56d89d1655d7
Bilal Akbar <bilal.akbar@kryptomind.com>
2022-01-10 16:19:34 +0500

    Added Cookies Sessions CSRF Protection

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,16 @@ import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 import { ProductsModule } from './products/products.module';
 import { ConsoleService } from './utils/console/console.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+  ],
   controllers: [AppController, ProductsController],
   providers: [AppService, ResponseService, ProductsService, ConsoleService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 import { ConsoleService } from './utils/console/console.service';
 import * as helmet from 'helmet';
 import * as csurf from 'csurf';
-
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
 declare const module: any;
 const consoleService = new ConsoleService();
 
@@ -12,8 +13,16 @@ async function bootstrap() {
   app.enableCors();
   app.enableVersioning();
   app.useGlobalInterceptors();
-  app.use(csurf());
-
+  app.use(csurf()); // Cross-site request forgery (also known as CSRF or XSRF)
+  app.use(cookieParser()); // For Cookies
+  app.use(
+    // for Session
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   // app.use(helmet());
   await app.listen(3000);
 
