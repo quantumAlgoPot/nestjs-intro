6748aa08e16474cec06d88c6f6bc8410b774dfc4
Bilal Akbar <bilal.akbar@kryptomind.com>
2022-01-10 16:03:14 +0500

    Added Web Pack Hot Reload and csurf for XML Attacks

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleService } from './utils/console/console.service';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+
+declare const module: any;
+const consoleService = new ConsoleService();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.enableVersioning();
+  app.useGlobalInterceptors();
+  app.use(csurf());
+
+  // app.use(helmet());
   await app.listen(3000);
+
+  if (module.hot) {
+    consoleService.print('Hot Reload in Use');
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
